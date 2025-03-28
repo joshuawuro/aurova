2ded4b66120d72463c93a7b4d444700933f57113
Joshuawuro <joshuawuro@gmail.com>
2025-03-28 13:56:17 +0000

    feat(hero) add hover effects and improve button interactions

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import TrueFocus from "./TrueFocus";
-import Image from "../assets/ON3S9518.png";
 import { BsSend } from "react-icons/bs";
 import { FiDownload } from "react-icons/fi";
 import { LuCodeXml } from "react-icons/lu";
+import Image from "../assets/ON3S9518.png";
 
 function Hero() {
+  const contactRef = useRef(null);
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  const scrollToSection = (ref) => {
+    ref.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
+
   return (
     <div className="flex flex-col lg:flex-row items-center lg:gap-10 justify-center min-h-screen max-w-7xl mx-auto text-white p-6 lg:p-16">
       <div className="lg:w-1/2 text-center lg:text-left">
@@ -26,14 +35,6 @@ function Hero() {
         >
           Hi, My Name is
         </h1>
-        {/* <TrueFocus
-          sentence="Joshua Awuro"
-          manualMode={false}
-          blurAmount={5}
-          borderColor="rgb(221, 255,0)"
-          animationDuration={1}
-          pauseBetweenAnimations={1}
-        /> */}
         <h1
           className="text-4xl lg:text-6xl font-bold text-lemon"
           data-aos="fade-up"
@@ -52,26 +53,28 @@ function Hero() {
         <div className="mt-6 flex gap-2 md:gap-4 items-center justify-center lg:justify-start">
           <a
             href="/contact"
-            className="bg-lemon text-black px-2 md:px-6 py-3 rounded-lg shadow-lg border-2 border-lemon cursor-pointer hover:bg-gray hover:text-lemon transition-all"
+            className="bg-lemon text-black px-2 md:px-6 py-3 rounded-lg shadow-lg border-2 border-lemon cursor-pointer transition-all 
+            hover:bg-gray-800 hover:text-lemon hover:scale-105"
             data-aos="fade-up"
             data-aos-duration="1900"
           >
             Contact me <BsSend className="inline-block ml-2" />
           </a>
           <a
-            href="/resume.pdf" // Replace with the actual resume link
-            download
-            className="bg-transparent border-2 border-lemon text-lemon px-2 md:px-5 py-3 rounded-lg shadow-lg hover:bg-lemon hover:text-black transition-all"
+            href="https://drive.google.com/file/d/1C_xtJQwSutnvUjOtyBZ25k1_NBESkoYm/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-transparent border-2 border-lemon text-lemon px-2 md:px-5 py-3 rounded-lg shadow-lg cursor-pointer transition-all
+            hover:bg-lemon hover:text-black hover:scale-105"
             data-aos="fade-up"
-            data-aos-duration="190"
+            data-aos-duration="1900"
           >
-            Download CV
-            <FiDownload className="inline-block ml-2" />
+            Download CV <FiDownload className="inline-block ml-2" />
           </a>
         </div>
       </div>
       <div
-        className=" mt-10 lg:mt-0"
+        className="mt-10 lg:mt-0"
         data-aos="fade-up"
         data-aos-duration="2000"
       >
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,4 +1,5 @@
 import IPG from "../assets/IPG.png";
+import colorgame from "../assets/colorgame.png";
 
 const projectData = [
   {
@@ -10,6 +11,18 @@ const projectData = [
     image: IPG,
     tags: ["REACT", "ASTRO", "TAILWINDCSS"],
     link: "https://development--ipgghana.netlify.app/",
+    animate: "fade-right",
+    animatespeed: "1000",
+  },
+  {
+    id: 2,
+    title: "Color Game",
+    description:
+      "A website for a political party in Nigeria. The website is built with React, Astro and TailwindCSS.",
+    year: "2025",
+    image: colorgame,
+    tags: ["REACT", "TAILWINDCSS"],
+    link: "https://hng-12-color-game.vercel.app/",
     animate: "fade-right",
     animatespeed: "1000",
   },
